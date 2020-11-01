52cc2c333ccf581366d2bf88440a095a09ea1fdf
Agit TM <agit.3mulya@gmail.com>
2020-11-01 14:49:14 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/mobpro_quiz_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobpro_quiz_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:mobpro_quiz_1/pages/edit_profile.dart';
+
+main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: EditProfile());
+  }
+}
