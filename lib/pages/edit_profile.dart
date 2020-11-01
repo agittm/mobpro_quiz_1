7b11a920729e7acabca0eb86547bb997edf7deba
Agit TM <agit.3mulya@gmail.com>
2020-11-01 15:56:32 +0700

    layout edit profile

--- lib/pages/edit_profile.dart
+++ lib/pages/edit_profile.dart
@@ -9,9 +9,43 @@ class _EditProfileState extends State<EditProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: CloseButton(
+          onPressed: () => onEditProfileClose(),
+        ),
+        title: Text('Edit Profile'),
+        centerTitle: true,
+      ),
       body: Container(
-        child: Center(child: Text('Edit Profile')),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Center(
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Input Name'),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              height: 48,
+              child: RaisedButton(
+                onPressed: () => onEditProfileClose(),
+                color: Colors.blue,
+                child: Text(
+                  'Save',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+        padding: EdgeInsets.only(top: 64, bottom: 64, left: 24, right: 24),
       ),
     );
+  }
+
+  void onEditProfileClose() {
+    Navigator.pop(context);
   }
 }
